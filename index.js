38b67009f4a520f1d52ff172d9adfd1f50b03000
Bhargab Kachari <108968675+Bhargab18-2001@users.noreply.github.com>
2022-08-23 23:33:19 +0530

    Dice Game
    Created a simple dice game using HTML, CSS, Javascript, DOM

---
+++ index.js
@@ -0,0 +1,22 @@
+//For leftdice:
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomDiceImage = "dice" + randomNumber1 + ".png";
+var randomImageSource = "images/" + randomDiceImage;
+var image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src", randomImageSource);
+
+//For rightdice
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+//For comment
+if (randomNumber1 > randomNumber2){
+  document.querySelector("h1").innerHTML = "🚩Player 1 wins";
+}
+else if (randomNumber2 > randomNumber1){
+  document.querySelector("h1").innerHTML = "Player 2 wins🚩";
+}
+else{
+  document.querySelector("h1").innerHTML = "Draw!";
+}
